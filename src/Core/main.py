0c7388010d9ee7ee82f8fabb0aa2661b88b4108e
Thor Malmby Jørgin <tjoer21@student.sdu.dk>
2023-11-16 10:44:23 +0100

    The cleaner lives

--- src/Core/main.py
+++ src/Core/main.py
@@ -9,7 +9,7 @@ print("fetch data")
 if os.environ["fetch"] == "True":
     df = pd.read_csv('http://datadump.cryptobot.dk/Major_Safety_Events.csv', low_memory=False)
 else:
-    df = pd.read_csv('Major_Safety_Events.csv')
+    df = pd.read_csv('src/Core/cleaned_output.csv')
 print("data fetched")
 print(df.columns[35])
 
@@ -21,7 +21,7 @@ server = app.server
 
 app.layout = html.Div([
     html.H1(children=TITLE, style={'textAlign': 'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Dropdown(df.Agency.unique(), 'Metro Transit', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
 
@@ -31,17 +31,16 @@ app.layout = html.Div([
     Input('dropdown-selection', 'value')
 )
 def update_graph(value: str) -> Figure:
-    return
-    # country_data = df[df.country == value]
-    # return go.
-    # return px.line(country_data, x='year', y='pop', labels={
-    #     "year": "years",
-    #     "pop": "population",
-    # })
+    country_data = df[df.Agency == value]
+    return px.scatter(country_data, x='Vehicle Speed', y='Total Serious Injuries', labels={
+        "Agency": "agency",
+        "Year": "years",
+    })
 
 
 def main():
     print(df)
+    print(df.columns)
     app.run(debug=True, host="127.0.0.1", port=8070)
 
 
