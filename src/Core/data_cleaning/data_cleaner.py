f0ea2f98fe3b4050aa6e2d3ced9f5373f0dd92ff
TobiasBak <tobib21@student.sdu.dk>
2023-12-18 19:02:14 +0100

    Added new columns (Month, State, Event Per Mil Citizens) + I removed rows where year = 2023
    Additionally I mapped agencies to years through a csv file I found

--- src/Core/data_cleaning/data_cleaner.py
+++ src/Core/data_cleaning/data_cleaner.py
@@ -1,5 +1,68 @@
-import pandas
+import pandas as pd
 import clean_counties
+
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
+
+# invert the dictionary
+abbrev_to_us_state = dict(map(reversed, us_state_to_abbrev.items()))
 
 
 def write_cleaned(filepath: str):
@@ -47,7 +110,10 @@ def main():
 
     index = 33
 
-    df = pandas.read_csv('../data/Major_Safety_Events.csv', low_memory=False)
+    df = pd.read_csv('../data/Major_Safety_Events.csv', low_memory=False)
+
+    df['Event Date'] = pd.to_datetime(df['Event Date'])
+    df['Event Time'] = pd.to_datetime(df['Event Time'])
 
     column_indexes_to_drop = [20, 24, 35, 39, 40, 43, 46, 47, 55]
     column_names_to_drop = []
@@ -61,6 +127,13 @@ def main():
     print("dropping columns")
     for name in column_names_to_drop:
         df.pop(name)
+
+    # Drop rows where Year is 2023
+    df = df[df['Year'] != 2023]
+
+    # df = add_month_column(df)
+    df = add_state_column(df)
+    df = add_event_divided_by_citizens(df)
 
     # print(df.info())
     for row in df.iterrows():
@@ -81,11 +154,59 @@ def main():
         print(f"{key}: {column_contents[key]}")
 
     df = cap_column(df, "Vehicle Speed", 200)
+
+    print(df)
+
     df.to_csv("../data/cleaned_output.csv", mode="w", encoding="utf8")
 
 
 def cap_column(df, column_name, cap):
     df.loc[df[column_name] > cap, column_name] = cap
+    return df
+
+
+def add_month_column(df: pd.DataFrame):
+    for index, row in df.iterrows():
+        df.at[index, 'Month'] = row['Event Date'].month_name()
+    return df
+
+
+# This method is dependent on add_state_column
+def add_event_divided_by_citizens(df: pd.DataFrame):
+    state_population = pd.read_csv(f"../data/states-population.csv")
+
+    for index, row in df.iterrows():
+        print(f"index: {index}")
+        state = row['State']
+        if state == "Unknown" or state == "Puerto Rico":
+            continue
+        year = row['Year']
+        population = state_population.loc[state_population['Years'] == str(state), str(year)].values[0]
+        df.at[index, 'Event Per Mil Citizens'] = 1000000 / population
+    return df
+
+
+def add_state_column(df: pd.DataFrame):
+    print("Adding state column")
+    agencies = pd.read_csv(f"../data/agencies.csv", encoding="UTF-8", sep=";")
+    data = pd.DataFrame()
+    data['NTD ID'] = agencies['NTD ID']
+    data['State'] = agencies['State']
+
+    for index, row in df.iterrows():
+        print(f"index: {index}")
+        agency_id = row['NTD ID']
+        results = data.loc[data['NTD ID'] == str(agency_id)]
+        if len(results) == 0:
+            df.at[index, 'State'] = "Unknown"
+        else:
+            df.at[index, 'State'] = abbrev_to_us_state[results.iloc[0]['State']]
+
+    # Use df.map instead of the for loop
+    # The input value for the key should be converted to string
+    # The output value should be converted to a list
+    # df['State'] = df['NTD ID'].map(lambda x: abbrev_to_us_state[data.loc[data['NTD ID'] == str(x)].iloc[0]['State']])
+
     return df
 
 
--- src/Core/data_provider.py
+++ src/Core/data_provider.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
 import numpy as np
+import united_states
 
 df = None
+
 
 def get_df():
     global df
@@ -12,12 +14,31 @@ def get_df():
         print("fetch data")
         df = pd.read_csv('http://datadump.cryptobot.dk/Major_Safety_Events.csv', low_memory=False)
     else:
-        filename = f"{__file__[:-(4+1+8+3)]}data/cleaned_output.csv"
+        filename = f"{__file__[:-(4 + 1 + 8 + 3)]}data/cleaned_output.csv"
         df = pd.read_csv(filename)
         # df.apply(lambda x: x['Event Date'].format(name=x['name']), axis=1)
         df['Event Date'] = pd.to_datetime(df['Event Date'])
         df['Event Time'] = pd.to_datetime(df['Event Time'])
+
+        # Add columns used in certain plots
+        df = add_event_divided_by_citizens(df) # This method is dependent on add_state_column
+
+        pd.DataFrame.to_csv(df, f"{__file__[:-(4 + 1 + 8 + 3)]}data/cleaned_output2.csv")
+
     print("data fetched")
     print(df.columns[12])
     print(df["Event Time"])
+    return df
+
+
+def add_event_divided_by_citizens(df: pd.DataFrame):
+    state_population = pd.read_csv(f"{__file__[:-(4 + 1 + 8 + 3)]}data/states-population.csv")
+    print("###########################################################")
+    print(state_population)
+
+    for index, row in df.iterrows():
+        state = row['State']
+        year = row['Year']
+        population = state_population.loc[state_population['Years'] == state, str(year)].values[0]
+        df.at[index, 'Event Divided By Citizens'] = row['Event Type Group'] / population
     return df
