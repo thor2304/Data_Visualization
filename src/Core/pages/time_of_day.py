ff4d6c5d2d363094b35a827bc4af25a9e367d167
Thor Malmby Jørgin <tjoer21@student.sdu.dk>
2023-12-19 15:35:51 +0100

    Time of day changed to bar chart

--- src/Core/pages/time_of_day.py
+++ src/Core/pages/time_of_day.py
@@ -2,7 +2,7 @@ import dash
 from dash import Dash, html, dcc, callback, Output, Input
 from plotly.graph_objs import Figure
 import plotly.express as px
-
+import numpy as np
 
 from src.Core.data_provider import get_df
 from src.Core.styles import graphDivStyle, pageStyle
@@ -11,35 +11,67 @@ dash.register_page(__name__)
 
 df = get_df()
 
+eventsListRaw = df["Event Type Group"].unique()
+eventsList = np.delete(eventsListRaw, np.where(eventsListRaw == "Non-RGX Collision"))
+
 layout = html.Div([
-    html.H1('Is there a relation between time periods in the day and certain types of accidents?', style={'textAlign': 'center', 'width': '60%'}),
+    html.H1('Is there a relation between time periods in the day and certain types of accidents?',
+            style={'textAlign': 'center', 'width': '60%'}),
     html.Div([
         html.Div([
-            dcc.Dropdown(df["Rail/Bus/Ferry"].unique(), 'Bus', id='3d-dropdown-selection', style={'width': '50%', 'justify-content': 'end'}),
-        ], style={'width': '100%', 'display': 'flex', 'justify-content': 'end', 'align-items': 'center'}),
-        dcc.Graph(id='3d-graph', style={'width': '100%', 'height': '60em'}),
+            html.H3(children="Choose event type", style={'textAlign': 'center'}),
+            dcc.Checklist(
+                id="event-checklist",
+                options=[{'label': html.Span(i, style={'padding-left': 10}), 'value': i} for i in eventsList],
+                value=[eventsList[0]],
+                inline=False,
+                labelStyle={'display': 'flex', 'align-items': 'center'},
+            ),
+            html.H3(children="Choose whether to normalise data", style={'textAlign': 'center', 'padding-top': 20}),
+            dcc.RadioItems(
+                id='event-normaliser',
+                options=["Normalise data", "Don't normalise data"],
+                value="Don't normalise data",
+                labelStyle={'display': 'flex'}),
+        ], style={'width': '60%', 'margin': 'auto', 'height': '100%'}),
+        dcc.Graph(id='time-graph', style={'width': '100%', 'height': '60em'}),
         html.H1('', style={'textAlign': 'center '}),
     ], style=graphDivStyle),
 ], style=pageStyle)
 
 
-
-# 3D GRAPH ###################################################################
 @callback(
-    Output('3d-graph', 'figure'),
-    Input('3d-dropdown-selection', 'value')
+    Output('time-graph', 'figure'),
+    Input('event-checklist', 'value')
 )
 def update_3d_plot(value: str) -> Figure:
-    category_data = df[df["Rail/Bus/Ferry"] == value]
-    marker_size = 3
+    # Filter the df so only the rows within dateStart and dateEnd are left
 
-    fig = px.scatter_3d(category_data, x='Event Date', y='Event Time', z='Total Injuries', labels={
-        "Agency": "agency",
-        "Year": "years",
-    }, color="Total Injuries")
+    # mask = (df['Event Date'] >= date_start) & (df['Event Date'] <= date_end)
+    # active_rows = df[mask]
 
-    fig.update_traces(marker=dict(
-        size=marker_size
-    ), selector=dict(mode="markers"))
+    mask = df['Event Type Group'].isin(value)
+    active_rows = df[mask]
+
+    active_rows['Hour'] = active_rows['Event Time'].apply(
+        lambda x: x.replace(second=0, microsecond=0, minute=0, hour=x.hour)
+    )
+
+    fig = px.histogram(active_rows, x="Hour", y="Total Injuries", color='Event Type Group', orientation='v',
+                       height=800,
+                       title="Test")
+
+    fig.update_xaxes(
+        title_text="Time of day"
+    )
 
     return fig
+
+# Plot 1: What ype of accidents at what time of day?
+# Stacked bar chart for accident types. X-axis: time of day. Y-axis: number of accidents. Color: accident type.
+
+# Plot 2:  At what time of day are people injured?
+# Bar chart for number of injuries. X-axis: time of day. Y-axis: number of injuries. Color: accident type.
+
+# Consider:
+# Overlaying the number of injuries as a line on the first plot.
