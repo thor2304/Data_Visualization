ec59bdabd7fcda209af6a4845b049e8d3e878b3f
Thor Malmby Jørgin <tjoer21@student.sdu.dk>
2023-12-20 20:04:41 +0100

    Slow transitions

--- src/Core/pages/time_of_day.py
+++ src/Core/pages/time_of_day.py
@@ -75,6 +75,37 @@ def time_of_day(y_selection: str, selected_event_types: list[str]) -> Figure:
                  animation_group="Hour",
                  )
 
+    # fig.update_layout(updatemenus=[
+    #     {
+    #         "buttons": [
+    #             {
+    #                 "args": [None, {"frame": {"duration": 500, "redraw": False},
+    #                                 "fromcurrent": True, "transition": {"duration": 300,
+    #                                                                     "easing": "quadratic-in-out"}}],
+    #                 "label": "Play",
+    #                 "method": "animate"
+    #             },
+    #             {
+    #                 "args": [[None], {"frame": {"duration": 0, "redraw": False},
+    #                                   "mode": "immediate",
+    #                                   "transition": {"duration": 0}}],
+    #                 "label": "Pause",
+    #                 "method": "animate"
+    #             }
+    #         ],
+    #         "direction": "left",
+    #         "pad": {"r": 10, "t": 87},
+    #         "showactive": False,
+    #         "type": "buttons",
+    #         "x": 0.1,
+    #         "xanchor": "right",
+    #         "y": 0,
+    #         "yanchor": "top"
+    #     }
+    # ]
+    # )
+
+
     return treat_histogram_fig(fig, y_selection)
 
 
@@ -127,7 +158,7 @@ def animated_time_of_day(y_selection: str, selected_event_types: list[str]) -> F
     return treat_histogram_fig(fig, y_selection)
 
 
-def treat_histogram_fig(fig: Figure, y_selection: str) -> Figure:
+def treat_histogram_fig(fig: Figure, y_selection: str, transition_duration = 1500) -> Figure:
     hovertemplate = '<b>%{data.name}</b><br>' + \
                     '<b>' + y_selection + ':</b> %{y}<br>' + \
                     '<b>Time:</b> %{x}<br>' + \
@@ -155,6 +186,9 @@ def treat_histogram_fig(fig: Figure, y_selection: str) -> Figure:
         hovermode='x unified',
         bargap=0.2,
     )
+
+    if len(fig.frames) > 1:
+        fig.layout.updatemenus[0].buttons[0].args[1]["frame"]["duration"] = transition_duration
 
     return fig
 
