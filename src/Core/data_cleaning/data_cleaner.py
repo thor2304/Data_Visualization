abde318b41d22aaf5b00b20829b6b91845973bf9
TobiasBak <tobib21@student.sdu.dk>
2023-12-20 23:57:23 +0100

    Fixed so percentages works

--- src/Core/data_cleaning/data_cleaner.py
+++ src/Core/data_cleaning/data_cleaner.py
@@ -131,18 +131,19 @@ def main():
 
     # Drop rows where Year is 2023
     df = df[df['Year'] != 2023]
+    df = df[df['Event Type Group'] != "Non-RGX Collision"]
+
 
     df = add_month_column(df)
     df = add_state_column(df)
     df = add_event_divided_by_citizens(df)
     add_columns_for_time_of_day(df)
-
-    df = df[df['Event Type Group'] != "Non-RGX Collision"]
+    add_percentage_out_of_total_accidents_per_state(df)
 
     # Drop rows where latitude is above 90
     df = df[df['Latitude'] < 90]
 
-    # Drop rows where longitude is above -45
+    # Drop rows where longitude is higher than -45
     df = df[df['Longitude'] < -45]
 
     df = cap_column(df, "Vehicle Speed", 200)
@@ -204,6 +205,15 @@ def add_state_column(df: pd.DataFrame):
     )
 
     return df
+
+def add_percentage_out_of_total_accidents_per_state(df: pd.DataFrame):
+    print("Adding percentage out of total accidents in state")
+    number_of_accidents_per_state = df.groupby("State", as_index=False).agg({"Number of accidents": sum})
+    df['Percentage of accidents in state'] = df.apply(
+        lambda x:
+        0 if x["State"] == "Unknown" or x["State"] == "Puerto Rico" else
+        x["Number of accidents"] / number_of_accidents_per_state.loc[number_of_accidents_per_state['State'] == x["State"], "Number of accidents"].values[0] * 100
+        , axis=1, )
 
 
 if __name__ == '__main__':
--- src/Core/pages/where_accidents.py
+++ src/Core/pages/where_accidents.py
@@ -7,7 +7,7 @@ import plotly.figure_factory as ff
 import dash_bootstrap_components as dbc
 
 from src.Core.CustomComponents import GraphDiv, CheckList
-from src.Core.data_provider import get_df
+from src.Core.data_provider import get_df, get_category_orders
 from src.Core.styles import graphStyle, dropdownStyle, legendColors
 
 dash.register_page(__name__, name="Where do accidents happen?")
@@ -18,7 +18,7 @@ eventsList = sorted(df["Event Type Group"].unique())
 
 layout = html.Div([
     html.H1('Do certain types of accidents occur more often in certain environments?',
-                style={'textAlign': 'center'}),
+            style={'textAlign': 'center'}),
     GraphDiv(
         left_of_graph=[
             dbc.Form([
@@ -38,6 +38,18 @@ layout = html.Div([
         graph=dcc.Graph(id='accident-map', style=graphStyle)
 
     ),
+    GraphDiv(
+        left_of_graph=[
+            html.Div(
+                dbc.RadioItems(
+                    options=eventsList,
+                    value=eventsList[0],
+                    id="bar_event_type",
+                ),
+                className="py-2",
+            )
+        ],
+        graph=dcc.Graph(id='top10-bar', style=graphStyle)),
 ], style={'display': 'flex', 'flex-direction': 'column', 'align-items': 'center'}, )
 
 
@@ -47,9 +59,15 @@ layout = html.Div([
     Input('map-event-checklist', 'value')
 )
 def update_map(value: int, event_type: str) -> Figure:
-
-    mask = df['Event Type Group'].isin(event_type)
+    mask = df['Event Type Group'].isin(event_type) & df['Latitude'].notna() & df['Longitude'].notna()
     active_rows = df[mask]
+    print(f"Number of rows: {len(active_rows)}")
+    # Drop rows where latitude is above 90
+    active_rows = active_rows[active_rows['Latitude'] < 90]
+
+    # Drop rows where longitude is higher than -45
+    active_rows = active_rows[active_rows['Longitude'] < -45]
+    print(f"Number of rows after dropping: {len(active_rows)}")
 
     min_count = 0
     opacity_value = 0.2
@@ -75,6 +93,40 @@ def update_map(value: int, event_type: str) -> Figure:
         range_color=[0, 200],
 
     )
+
+    return fig
+
+
+@callback(
+    Output('top10-bar', 'figure'),
+    Input('bar_event_type', 'value')
+)
+def update_bar(bar_event_type: str) -> Figure:
+    # Remove Unknown from the dataframe
+
+    # Create a mask to remove all rows where the event type is unknown
+    mask = (df['Event Type Group'] != "Unknown")
+    newDf = df[mask]
+    print(newDf)
+
+    # sum the percentage of accidents in Alabame for each event type
+    alabama_df = newDf[newDf["State"] == "Louisiana"]
+    alabama_df = alabama_df.groupby(["Event Type Group"], as_index=False).agg({"Percentage of accidents in state": sum})
+    print(alabama_df)
+
+    # get top 10 states with the highest percentage of accidents for the selected event type out of all event types
+    grouped_df = newDf.groupby(["State", "Event Type Group"], as_index=False).agg({"Percentage of accidents in state": sum})
+    grouped_df = grouped_df.sort_values("Percentage of accidents in state", ascending=False)
+    grouped_df = grouped_df[grouped_df["Event Type Group"] == bar_event_type]
+    grouped_df = grouped_df.head(10)
+
+    fig = px.bar(grouped_df, x="State", y="Percentage of accidents in state", color='Event Type Group',
+                 category_orders=get_category_orders(),
+                 color_discrete_sequence=legendColors,
+                 orientation='v',
+                 )
+
+    fig.update_yaxes(ticksuffix="%")
 
 
 
