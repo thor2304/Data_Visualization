b265f8cd28e21029a56b35825c9ece1bd355bf19
Thor Malmby Jørgin <tjoer21@student.sdu.dk>
2023-12-21 00:32:23 +0100

    Includes nan latitude longitude when filtering

--- src/Core/data_cleaning/data_cleaner.py
+++ src/Core/data_cleaning/data_cleaner.py
@@ -133,7 +133,6 @@ def main():
     df = df[df['Year'] != 2023]
     df = df[df['Event Type Group'] != "Non-RGX Collision"]
 
-
     df = add_month_column(df)
     df = add_state_column(df)
     df = add_event_divided_by_citizens(df)
@@ -141,10 +140,10 @@ def main():
     add_percentage_out_of_total_accidents_per_state(df)
 
     # Drop rows where latitude is above 90
-    df = df[df['Latitude'] < 90]
+    df = df[(df['Latitude'] < 90) | (df['Latitude'].isna())]
 
     # Drop rows where longitude is higher than -45
-    df = df[df['Longitude'] < -45]
+    df = df[(df['Longitude'] < -45) | (df['Longitude'].isna())]
 
     df = cap_column(df, "Vehicle Speed", 200)
 
@@ -206,13 +205,15 @@ def add_state_column(df: pd.DataFrame):
 
     return df
 
+
 def add_percentage_out_of_total_accidents_per_state(df: pd.DataFrame):
     print("Adding percentage out of total accidents in state")
     number_of_accidents_per_state = df.groupby("State", as_index=False).agg({"Number of accidents": sum})
     df['Percentage of accidents in state'] = df.apply(
         lambda x:
         0 if x["State"] == "Unknown" or x["State"] == "Puerto Rico" else
-        x["Number of accidents"] / number_of_accidents_per_state.loc[number_of_accidents_per_state['State'] == x["State"], "Number of accidents"].values[0] * 100
+        x["Number of accidents"] / number_of_accidents_per_state.loc[
+            number_of_accidents_per_state['State'] == x["State"], "Number of accidents"].values[0] * 100
         , axis=1, )
 
 
